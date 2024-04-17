4671ea8eeb8d378817f0d1aa874d03d409681f6a
stanislaw_jakiel <stanislaw.dev@gmail.com>
2024-04-17 20:25:53 +0200

    test env setup experimenting with kaniko go-to setup

--- src/main/scala/io/github/kiemlicz/kraftsman/KanikoPlugin.scala
+++ src/main/scala/io/github/kiemlicz/kraftsman/KanikoPlugin.scala
@@ -1,9 +1,11 @@
 package io.github.kiemlicz.kraftsman
 
-import com.typesafe.sbt.packager.Keys.{dockerExecCommand, stage}
+import com.typesafe.sbt.packager.Keys.stage
 import com.typesafe.sbt.packager.docker.DockerPlugin
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.Docker
-import sbt.{AutoPlugin, Configuration, Setting, inConfig, taskKey}
+import sbt.{AutoPlugin, Configuration, Setting, inConfig, settingKey, taskKey}
+
+import java.net.URI
 
 object KanikoPlugin extends AutoPlugin {
   override def trigger = noTrigger
@@ -11,12 +13,14 @@ object KanikoPlugin extends AutoPlugin {
   override def requires = DockerPlugin
 
   object autoImport {
+    lazy val kanikoContextLocation = settingKey[URI]("Location of the Docker context")
     lazy val kanikoPublishLocal = taskKey[Unit]("Builds a Docker image using Kaniko POC")
   }
 
   import autoImport.*
 
   lazy val baseKanikoSettings: Seq[Setting[_]] = Seq(
+    kanikoContextLocation := ???,
     kanikoPublishLocal := {
       val st = stage.value
       // run_in_docker
